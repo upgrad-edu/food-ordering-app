cd6dc31f84606625feb582f74f34b63c1c9260a2
SonaliBhagwatkar <sonali.bhagwatkar@gmail.com>
2019-09-25 01:09:13 +0530

    Created Home component and initialized that as an index page

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import Home from './screens/home/Home';
 
-ReactDOM.render(
-    <div>
-        Food Ordering App
-    </div>,
-    document.getElementById('root')
+ReactDOM.render( <Home/> , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/screens/home/Home.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Home.css';
+
+class Home extends Component {
+    render(){
+        return(
+            <div>Home Page</div>
+        )
+    }
+}
+
+export default Home;
