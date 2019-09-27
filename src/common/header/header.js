d6c15effd335c257bf6aa71934a87783f73b1e6c
SonaliBhagwatkar <sonali.bhagwatkar@gmail.com>
2019-09-27 12:30:53 +0530

    Added Login form with LOGIN button

--- src/common/header/header.js
+++ src/common/header/header.js
@@ -8,6 +8,14 @@ import SearchIcon from '@material-ui/icons/Search';
 import Input from '@material-ui/core/Input';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import {withStyles} from '@material-ui/core';
+import Modal from 'react-modal';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import PropTypes from 'prop-types';
+
 
 
 const styles = theme => ({
@@ -18,10 +26,59 @@ const styles = theme => ({
         "&::after": {
             borderBottom: "1px solid white"
           }
+    },
+    formControl: {
+        width: "85%",
     }
 });
 
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+}
+
+const TabContainer = function(props){
+    return(
+        <Typography component="div" style={{padding: 0, textAlign: 'center'}}>
+            {props.children}
+        </Typography>
+    );
+}
+
+TabContainer.propTypes = {
+    children: PropTypes.node.isRequired
+}
+
+
+
 class Header extends Component {
+    constructor()
+    {
+        super();
+        this.state = {
+            modalIsOpen: false,
+            value: 0
+        }
+    }
+
+    openModalHandler = () => {
+        this.setState({modalIsOpen:true})
+    }
+
+    closeModalHandler = () => {
+        this.setState({modalIsOpen:false})
+    }
+
+    tabChangeHandler = (event, value) => {
+        this.setState({value})
+    }
+
     render() {
         const {classes} = this.props;
             return (
@@ -49,7 +106,7 @@ class Header extends Component {
                         />
                     </div>
                     <div className="login-button">
-                        <Button variant="contained" color="default">
+                        <Button variant="contained" color="default" onClick={this.openModalHandler}>
                             <SvgIcon>
                                 <AccountCircleIcon />
                             </SvgIcon>
@@ -57,6 +114,30 @@ class Header extends Component {
                         </Button>
                     </div>
                 </header>
+                <Modal 
+                    ariaHideApp={false}  
+                    isOpen={this.state.modalIsOpen} 
+                    contentLabel="Login"
+                    onRequestClose={this.closeModalHandler}
+                    style={customStyles}>
+                  <Tabs className="tabs" value={this.state.value} onChange={this.tabChangeHandler}>
+                      <Tab label="Login"/>
+                      <Tab label="Signup"/>
+                  </Tabs>
+                  {this.state.value === 0 &&
+                  <TabContainer>
+                      <FormControl required className={classes.formControl}>
+                          <InputLabel htmlFor="contactno">Contact No</InputLabel>
+                          <Input id="contactno" type="text" />
+                      </FormControl><br/><br/>
+                      <FormControl required className={classes.formControl}>
+                          <InputLabel htmlFor="password">Password</InputLabel>
+                          <Input id="password" type="password" />
+                      </FormControl><br/>
+                      <br/><br/>
+                      <Button variant="contained" color="primary">LOGIN</Button>
+                  </TabContainer>}
+                </Modal>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'typeface-roboto';
 import * as serviceWorker from './serviceWorker';
 import Home from './screens/home/Home';
 
