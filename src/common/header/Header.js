af3bc86d04ebe01da9e4066f54d6c7623ef6bb0a
Rahul <rahul7751@gmail.com>
2019-12-20 11:17:27 +0530

    header code completed will all the icons

---
+++ src/common/header/Header.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './Header.css';
+import Button from '@material-ui/core/Button';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import FastfoodIcon from '@material-ui/icons/Fastfood';
+import SearchIcon from '@material-ui/icons/Search';
+import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import {withStyles} from '@material-ui/core';
+
+const styles = theme => ({
+    searchBox: {
+        color: "#fff",
+        width: "320px",
+        fontSize: 15,
+        "&::after": {
+            borderBottom: "1px solid white"
+          }
+    }
+});
+
+class Header extends Component {
+    render() {
+            const {classes} = this.props;
+            return (
+                //This code section implements the LOGIN button part of the header
+                    <div>
+                        <header className="app-header">
+                        <div className="logo">
+                            <SvgIcon className="app-logo">
+                                <FastfoodIcon/>
+                            </SvgIcon>
+                        </div>
+                        <div className="search-box">
+                            <Input
+                                className={classes.searchBox}
+                                id="input-with-icon-adornment"
+                                startAdornment={
+                                    <InputAdornment position="start" className="search">
+                                        <SvgIcon>
+                                             <SearchIcon/>
+                                        </SvgIcon>
+                                    </InputAdornment>
+                                }
+                                placeholder="Search by Restaurant Name"
+
+                            />
+                     </div>
+                    <div className="login-button">
+                        <Button variant="contained" color="default">
+                            <SvgIcon>
+                                <AccountCircleIcon />
+                            </SvgIcon>
+                            <span className="login-spacing">LOGIN</span>
+                        </Button>
+                    </div>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default withStyles(styles)(Header);
---
+++ src/screens/home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Header from '../../common/header/Header';
+
+class Home extends Component {
+    render() {
+        return (
+              <div>
+                  <Header/>
+              </div>
+        )
+    }
+}
+export default Home;
