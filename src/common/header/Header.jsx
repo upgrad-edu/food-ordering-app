55b7d7090326ac00659c184e81e834f245b8c0d1
Kabir <kabir4691@gmail.com>
2020-08-28 05:32:46 +0530

    Add search bar to header

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
 import './Header.css'
+import { withStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import Fastfood from '@material-ui/icons/Fastfood'
+import Search from '@material-ui/icons/Search';
+
+const classes = theme => ({
+  searchInput: {
+    color: 'white'
+  },
+  searchIcon: {
+    height: '28px',
+  }
+})
 
 class Header extends Component {
 
@@ -10,9 +23,22 @@ class Header extends Component {
         <div className="app-logo">
           <Fastfood fontSize="large" htmlColor="white"/>
         </div>
+        <div className="search-container">
+          <Input 
+            className={classes.searchInput}
+            startAdornment={
+              <InputAdornment position="start">
+                <Search className={classes.searchIcon} htmlColor="white"/>
+              </InputAdornment>
+            }
+            fullWidth={true} 
+            placeholder="Search by Restaurant Name"
+            color="primary"
+          />
+        </div>
       </header>
     )
   }
 }
 
-export default Header
+export default withStyles(classes)(Header)
