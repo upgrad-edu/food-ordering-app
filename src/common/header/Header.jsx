781c9f3f6bb5f8fe4328dec9c487504eede06b99
Kabir <kabir4691@gmail.com>
2020-08-28 08:44:33 +0530

    Add modal for signup

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -41,10 +41,22 @@ class Header extends Component {
     this.state = {
       isModalOpen: false,
       tabsValue: 0,
-      contactNumber: "",
-      contactNumberError: false,
-      passwordI: "",
-      passwordError: false
+      loginContactNumber: "",
+      loginContactNumberError: false,
+      loginPassword: "",
+      loginPasswordError: false,
+      firstName: "",
+      firstNameError: false,
+      lastName: "",
+      email: "",
+      emailError: false,
+      signupPassword: "",
+      signupPasswordRequiredError: false,
+      signupPasswordValidationError: false,
+      signupContactNumber: "",
+      signupContactNumberRequiredError: false,
+      signupContactNumberValidationError: false,
+      signupContactNumberRegisteredError: false
     }
   }
 
@@ -60,12 +72,32 @@ class Header extends Component {
     this.setState({ tabsValue: value });
   }
 
-  contactNumberInputHandler = (event) => {
-    this.setState({ contactNumber: event.target.value })
+  loginContactNumberInputHandler = (event) => {
+    this.setState({ loginContactNumber: event.target.value });
   }
 
-  passwordInputHandler = (event) => {
-    this.setState({ password: event.target.value })
+  loginPasswordInputHandler = (event) => {
+    this.setState({ loginPassword: event.target.value });
+  }
+
+  firstNameInputHandler = (event) => {
+    this.setState({ firstName: event.target.value });
+  }
+
+  lastNameInputHandler = (event) => {
+    this.setState({ lastName: event.target.value });
+  }
+
+  emailInputHandler = (event) => {
+    this.setState({ email: event.target.value });
+  }
+
+  signupPasswordInputHandler = (event) => {
+    this.setState({ signupPassword: event.target.value });
+  }
+
+  signupContactNumberInputHandler = (event) => {
+    this.setState({ signupContactNumber: event.target.value });
   }
 
   render() {
@@ -106,26 +138,26 @@ class Header extends Component {
             <div className="modal-content">
               <br />
               <FormControl fullWidth required>
-                <InputLabel htmlFor="contact-number">Contact No.</InputLabel>
+                <InputLabel>Contact No.</InputLabel>
                 <Input
                   className="input-fields"
                   type="tel"
-                  value={this.state.contactNumber}
-                  onChange={this.contactNumberInputHandler}/>
-                <FormHelperText className={this.state.contactNumberError ? "dispBlock" : "dispNone"}>
+                  value={this.state.loginContactNumber}
+                  onChange={this.loginContactNumberInputHandler}/>
+                <FormHelperText className={this.state.loginContactNumberError ? "dispBlock" : "dispNone"}>
                   <span className='red'>required</span>
                 </FormHelperText>
               </FormControl>
               <br />
               <br />
               <FormControl fullWidth required>
-                <InputLabel htmlFor="password">Password</InputLabel>
+                <InputLabel>Password</InputLabel>
                 <Input
                   className="input-fields"
                   type="password"
-                  value={this.state.password}
-                  onChange={this.passwordInputHandler} />
-                <FormHelperText className={this.state.passwordError ? "dispBlock" : "dispNone"}>
+                  value={this.state.loginPassword}
+                  onChange={this.loginPasswordInputHandler} />
+                <FormHelperText className={this.state.loginPasswordError ? "dispBlock" : "dispNone"}>
                   <span className='red'>required</span>
                 </FormHelperText>
               </FormControl>
@@ -134,6 +166,85 @@ class Header extends Component {
               <br/>
               <Button variant="contained" color="primary">
                 LOGIN
+              </Button>
+            </div>
+          }
+          { this.state.tabsValue === 1 &&
+            <div className="modal-content">
+              <br />
+              <FormControl fullWidth required>
+                <InputLabel>First Name</InputLabel>
+                <Input
+                  className="input-fields"
+                  type="text"
+                  value={this.state.firstName}
+                  onChange={this.firstNameInputHandler} />
+                <FormHelperText className={this.state.firstNameError ? "dispBlock" : "dispNone"}>
+                  <span className="red">required</span>
+                </FormHelperText>
+              </FormControl>
+              <br/>
+              <br/>
+              <FormControl fullWidth>
+                <InputLabel>Last Name</InputLabel>
+                <Input 
+                  className="input-fields"
+                  type="text"
+                  value={this.state.lastName} 
+                  onChange={this.lastNameInputHandler} />
+              </FormControl>
+              <br/>
+              <br/>
+              <FormControl fullWidth required>
+                <InputLabel>Email</InputLabel>
+                <Input 
+                  className="input-fields"
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.emailInputHandler} />
+                <FormHelperText className={this.state.emailError ? "dispBlock" : "dispNone"}>
+                  <span className="red">required</span>
+                </FormHelperText>
+              </FormControl>
+              <br/>
+              <br/>
+              <FormControl fullWidth required>
+                <InputLabel>Password</InputLabel>
+                <Input
+                  className="input-fields"
+                  type="password"
+                  value={this.state.signupPassword}
+                  onChange={this.signupPasswordInputHandler} />
+                <FormHelperText className={this.state.signupPasswordRequiredError ? "dispBlock" : "dispNone"}>
+                    <span className="red">required</span>
+                </FormHelperText>
+                <FormHelperText className={this.state.signupPasswordValidationError ? "dispBlock" : "dispNone"}>
+                  <span className="red">Password must contain at least one capital letter, one small letter, one number, and one special character</span>
+                </FormHelperText>
+              </FormControl>
+              <br/>
+              <br/>
+              <FormControl fullWidth required>
+                <InputLabel>Contact No.</InputLabel>
+                <Input 
+                  className="input-fields"
+                  value={this.state.signupContactNumber}
+                  onChange={this.signupContactNumberInputHandler} />
+                <FormHelperText className={this.state.signupContactNumberRequiredError ? "dispBlock" : "dispNone"}>
+                  <span className="red">required</span>
+                </FormHelperText>
+                <FormHelperText className={this.state.signupContactNumberValidationError ? "dispBlock" : "dispNone"}>
+                  <span className="red">Contact No. must contain only numbers and must be 10 digits long</span>
+                </FormHelperText>
+                <FormHelperText className={this.state.signupContactNumberRegisteredError ? "dispBlock" : "dispNone"}>
+                  <span className="red">This contact number is already registered! Try other contact number.</span>
+                </FormHelperText>
+              </FormControl>
+              <br/>
+              <br/>
+              <br/>
+              <Button variant="contained" color="primary">
+                SIGNUP
               </Button>
             </div>
           }
