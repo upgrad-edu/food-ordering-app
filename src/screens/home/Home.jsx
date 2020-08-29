c2db8344c89a1a8ee20930bc8883208588a0eadf
Pradeep Hegde <pradeephegde2488@gmail.com>
2020-08-29 15:45:39 +0530

    Update Search Restaurant and Rendering functionalities for the home page

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
+import Header from '../../common/header/Header';
+
+import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import 'font-awesome/css/font-awesome.min.css';
 import '@fortawesome/fontawesome-free-solid';
@@ -35,6 +43,103 @@ class Home extends Component {
     })
     xhrRestaurant.open("GET", this.props.baseUrl + "restaurant") // Getting all data from the restaurant endpoint.
     xhrRestaurant.send(data)
+  }
+
+
+  //This method is called from the header when the search box is activated and the page is needed to be updated with the new restaurant list.
+  updateSearchRestaurant = (searchRestaurant, searchOn) => {
+    let allRestaurantData = [];
+    if (searchOn) {
+      if (!this.state.isSearchOn) {
+        allRestaurantData = this.state.restaurant;
+        this.setState({
+          restaurant: searchRestaurant,
+          allRestaurantData: allRestaurantData,
+          isSearchOn: true,
+        })
+      } else {
+        this.setState({
+          ...this.state,
+          restaurant: searchRestaurant,
+        })
+      }
+    } else {
+      allRestaurantData = this.state.allRestaurantData;
+      this.setState({
+        restaurant: allRestaurantData,
+        isSearchOn: false,
+      });
+    }
+  }
+
+
+  //This Method Handles the click on the restaurant card.
+  //It takes the restaurant id as the Parameter and then pushes the restaurantDetails page
+  restaurantCardClicked = (restaurantId) => {
+    this.props.history.push('/restaurant/' + restaurantId);
+  }
+
+  render() {
+    // Styles are stored in the const classes
+    const { classes } = this.props;
+    return (
+        <div>
+          {/* Rendering the Header Component */}
+          <Header baseUrl={this.props.baseUrl} showHeaderSearchBox={true} updateSearchRestaurant={this.updateSearchRestaurant}></Header>
+          <div className="flex-container">
+            <Grid container spacing={3} wrap="wrap" alignContent="center" className={classes.grid}>
+              {/* Restaurant array map method is called and for each item in the array a card is rendered with the details. */}
+              {this.state.restaurant !== null ? this.state.restaurant.map(restaurant => (
+                      //Grid Used to create columns as per the screen size
+                      //Card used to show the restaurant details.
+                      <Grid key={restaurant.id} item xs={12} sm={6} md={3} className={classes.gridCard}>
+                        <Card className={classes.card}>
+                          <CardActionArea className={classes.cardActionArea} onClick={() => this.restaurantCardClicked(restaurant.id)}>
+                            <CardMedia
+                                className={classes.media}
+                                image={restaurant.photo_URL}
+                                title={restaurant.restaurant_name}
+                            />
+                            <CardContent className={classes.cardContent}>
+                              <Typography className={classes.title} variant="h5" component="h2">
+                                {restaurant.restaurant_name}
+                              </Typography>
+                            </CardContent>
+                            <CardContent className={classes.cardContent}>
+                              <Typography variant="subtitle1" component="p" className={classes.categories}>
+                                {restaurant.categories}
+                              </Typography>
+                            </CardContent>
+                            <CardContent className={classes.cardContent}>
+                              <div className="card-bottom-info">
+                                                <span className="rest-rating">
+                                                    <span>
+                                                        <FontAwesomeIcon icon="star" size="lg" color="white" />
+                                                    </span>
+                                                    <Typography variant="caption" component="p" >{restaurant.customer_rating}</Typography>
+                                                    <Typography variant="caption" component="p" >({restaurant.number_customers_rated})</Typography>
+                                                </span>
+                                <span className="rest-for-two">
+                                                    <Typography variant="caption" component="p" style={{fontSize: '14px'}}>
+                                                        <i className="fa fa-inr" aria-hidden="true"></i>
+                                                      {restaurant.average_price}
+                                                    </Typography>
+                                                    <Typography variant="caption" component="p" style={{fontSize: '14px'}}>for two</Typography>
+                                                </span>
+                              </div>
+                            </CardContent>
+                          </CardActionArea>
+                        </Card>
+                      </Grid>
+                  ))
+                  :<Typography variant='body1' component='p'>
+                    No restaurant with given name.
+                  </Typography>
+              }
+            </Grid>
+          </div>
+        </div>
+    )
   }
 }
 
