e079e07a0770f5fca49092df2a4c155d82b7edf8
ripansharma <ripan.may@gmail.com>
2021-07-26 14:40:02 +0530

    material ui core and initial changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import FoodOrdering from './FoodOrdering';
+import 'typeface-roboto';
+import {BrowserRouter as Router} from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <div>
-        Food Ordering App
-    </div>,
+    <Router>
+        <FoodOrdering />
+    </Router>,
     document.getElementById('root')
 );
 
